25e8b5d18097b7663c742c68f68efcaa511e92c7
nikhil7011 <er.nikhilmeena@gmail.com>
2021-09-17 15:42:03 +0530

    RMX1851: bump version to 3.9 | Eternal

--- aosp_RMX1851.mk
+++ aosp_RMX1851.mk
@@ -36,8 +36,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # PixelPlusUI OFFICIAL
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.ppui.device_name=Realme 3 Pro \
-    ro.ppui.version=3.8 \
-    ro.ppui.version_code=Hikaru \
+    ro.ppui.version=3.9 \
+    ro.ppui.version_code=Eternal \
     ro.ppui.is_official=true \
     ro.ppui.maintainer_name=RealmeDiary
 
